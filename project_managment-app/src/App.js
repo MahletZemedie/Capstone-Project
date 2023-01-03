bb606097887205425a8a12ff7e74c86f6d19bc9b
Mahlet Zemedie <mahletzemedie@gmail.com>
2023-01-03 15:36:25 -0600

    Change login page form

--- project_managment-app/src/App.js
+++ project_managment-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import ProjectManagementApp, {LoginComponent} from './Components/ProjectManagement/ProjectManagementApp';
+
  
 class App extends Component {
   render() {
